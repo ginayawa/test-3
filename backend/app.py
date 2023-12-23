e8648be0546c3858e206e995cd59bcfee2d28484
ginayawa <dsawayanagi@icloud.com>
2023-12-23 09:23:56 +0900

    frontend change

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,16 @@ stores = [
 ]
 
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route("/store")
 def get_stores():
   return {"stores":stores}
+
+@app.route("/fetchtest")
+def fetchtest():
+  res = requests.get('https://jsonplaceholder.typicode.com/users')
+  return res.json(), 200
 
 
--- frontend/my-app/src/app/page.jsx
+++ frontend/my-app/src/app/page.jsx
@@ -1,7 +1,20 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
-  )
+    <main>
+      <h1 className="m-6 text-4xl text-center">
+        <p>WebAPI Test App</p>
+        <p className="text-2xl">(Blog API from jsonplaceholder)</p>
+      </h1>
+
+      <div className="flex justify-center">
+        <Link href="/Blog">
+          <div className="cursor-pointer hover:bg-gray-200 border-2 rounded p-4">
+            <p>Blogへ移動</p>
+          </div>
+        </Link>
+      </div>
+    </main>
+  );
 }
